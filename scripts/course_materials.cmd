b95093e3a1dbcade91b858b109ed786ad91ede41
James O'Reilly <jp.oreilly@hotmail.com>
2018-03-17 16:52:54 +0000

    update

--- scripts/course_materials.cmd
+++ scripts/course_materials.cmd
@@ -8,19 +8,18 @@ echo. If you have already executed this script:
 echo. **Just the selected Pentaho Course Course Folder is deleted**
 echo.
 echo.    1. Business Analytics User Console - BA-1000
-echo.       *BA-1000 - Course Files installed to C:\Pentaho Training\BA-1000
+echo.        *BA-1000 - Course Files installed to C:\Pentaho Training\BA-1000
 echo.
 echo.    2. Business Analytics Report Designer - BA-2000
-echo.       *BA-2000 - Course Files installed to C:\Pentaho Training\BA-2000
+echo.        *BA-2000 - Course Files installed to C:\Pentaho Training\BA-2000
 echo.
 echo.    3. Business Analytics Data Modelling - BA-3000
-echo.       *BA-2000 - Course Files installed to C:\Pentaho Training\BA-3000
+echo.        *BA-3000 - Course Files installed to C:\Pentaho Training\BA-3000
 echo.
 echo.    4. Pentaho Data Integration - DI-1000
-echo.       *DI-1000 - Course Files installed to C:\Pentaho Training\DI-1000
+echo.        *DI-1000 - Course Files installed to C:\Pentaho Training\DI-1000
 echo.
-echo.
-echo.    4. Advanced Pentaho Data Integration - DI-1500
+echo.    5. Advanced Pentaho Data Integration - DI-1500
 echo.        *DI-1500 - Course Files installed to C:\Pentaho Training\DI-1500
 echo.        *Projects - Project files installed to C:\Projects
 echo.
@@ -58,7 +57,7 @@ EXIT
 
 :Item_3
 SET DIR="C:\Pentaho Training - Test\"
-IF EXIST %DIR% RMDIR /S /Q %DIR%\BA-2000
+IF EXIST %DIR% RMDIR /S /Q %DIR%\BA-3000
 GOTO :NOFOLDER
 IF NOT EXIST %DIR% GOTO NOFOLDER
 :NOFOLDER
@@ -70,23 +69,32 @@ EXIT
 
 :Item_4
 SET DIR="C:\Pentaho Training - Test\"
-IF EXIST %DIR% RMDIR /S /Q %DIR%\BA-2000
+IF EXIST %DIR% RMDIR /S /Q %DIR%\DI-1000
 GOTO :NOFOLDER
 IF NOT EXIST %DIR% GOTO NOFOLDER
 :NOFOLDER
 MD %DIR%
 CD %DIR%
-git clone https://github.com/jporeilly/DI-1500.git BA-3000
+git clone https://github.com/jporeilly/DI-1500.git DI-1000
 EXIT
 
 
 
 :Item_5
+SET DIR="Confluent-4.0 - Test\"
+IF EXIST %DIR% RMDIR /S /Q %DIR%\Confluent-4.0
+GOTO :NOFOLDER
+IF NOT EXIST %DIR% GOTO NOFOLDER
+:NOFOLDER
+MD %DIR%
+CD %DIR%
+git clone https://github.com/jporeilly/DI-1500.git Confluent-4.0
+
 SET DIR="C:\Pentaho Training - Test\"
 IF EXIST %DIR% RMDIR /S /Q %DIR%\DI-1500
 GOTO :NOTEST
-IF NOT EXIST %DIR% GOTO NOTEST
-:NOTEST
+IF NOT EXIST %DIR% GOTO NOFOLDER
+:NOFOLDER
 MD %DIR%
 CD %DIR%
 git clone https://github.com/jporeilly/DI-1500.git DI-1500
@@ -94,9 +102,9 @@ git clone https://github.com/jporeilly/DI-1500.git DI-1500
 SET DIR="C:\Projects  - Test\"
 IF EXIST %DIR% RMDIR /S /Q %DIR%
 GOTO :NOTEST2
-IF NOT EXIST %DIR% GOTO NOTEST2
-:NOTEST2
+IF NOT EXIST %DIR% GOTO NOFOLDER
+:NOFOLDER
 MD %DIR%
 CD %DIR%
-git clone https://github.com/jporeilly/DI-1500.git DI-1500
+git clone https://github.com/jporeilly/DI-1500.git Projects
 EXIT
