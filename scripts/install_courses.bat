00813a4829024d1ad60b6b6edd7952a8ab9b0662
James <jp.oreilly@hotmail.com>
2018-03-17 05:52:16 +0000

    updates

---
+++ scripts/install_courses.bat
@@ -0,0 +1,28 @@
+@echo off
+cls
+:start
+echo.
+echo. This script installs the required Course Materials.
+echo.
+echo.
+echo. 4. Advanced Pentaho Data Integration - DI-1500
+echo.
+echo.    The script connects to  GitHub and clones DI-1500 repositories:
+echo.     DI-1500 - Course 
+echo.     Projects - Project files for the workflows
+echo.   
+echo.   
+echo.
+echo. The window will close once scripts have successfully executed.
+echo.
+echo.
+set /p x= Select an Option:
+IF '%x%' == '%x%' GOTO Item_%x%
+
+
+
+:Item_4
+mkdir Test
+cd /Test/
+git clone https://github.com/jporeilly/DI-1500.git DI-1500
+GOTO Start
