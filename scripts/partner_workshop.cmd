07cb006d1d5bbca1c8094f0726c5d2bbf7ec9405
James O'Reilly <jp.oreilly@hotmail.com>
2018-05-11 14:24:24 +0100

    Update workshop cmd

--- scripts/partner_workshop.cmd
+++ scripts/partner_workshop.cmd
@@ -2,7 +2,8 @@
 cls
 :start
 echo.
-echo. This script installs the required Course Materials from GitHub Repositories.
+echo. Select an option to install the required Course Materials from GitHub Repositories.
+echo. Requires GIT to be installed and configured
 echo.
 echo. 
 echo.    1. Partner Workshop - Day 1
@@ -18,10 +19,15 @@ echo.          sls_raw_agile database
 echo.
 echo.    3. Partner Workshop - Day 3
 echo.        *Course Files installed to C:\Partner_Workshop\Day3
+echo.          sls_dma database
+echo.          Metadata Model
+echo.          Mondrian Schema
+echo.          PRD Reports
 echo.
 echo.    4. Partner Workshop - Day 4
 echo.        *Course Files installed to C:\Partner_Workshop\Day4
-echo.
+echo.          Dashboards
+echo.          Testing
 echo.
 echo.        
 echo.    5. Partner Workshop ...    
@@ -35,63 +41,60 @@ IF '%x%' == '%x%' GOTO Item_%x%
 
 
 :Item_1
-SET DIR="C:\Partner_Workshop\"
+SET DIR="C:\Partner_Workshop\Day1"
 IF EXIST %DIR% RMDIR /S /Q %DIR%\Day1
 GOTO :NOFOLDER
 IF NOT EXIST %DIR% GOTO NOFOLDER
 :NOFOLDER
 MD %DIR%
 CD %DIR%
-git clone https://github.com/jporeilly/Partner_Workshop.git Day1
+git clone -b Day1 https://github.com/jporeilly/Partner-Workshop.git .
 EXIT
 
 
 :Item_2
-SET DIR="C:\Partner_Workshop\"
+SET DIR="C:\Partner_Workshop\Day2"
 IF EXIST %DIR% RMDIR /S /Q %DIR%\Day2
 GOTO :NOFOLDER
 IF NOT EXIST %DIR% GOTO NOFOLDER
 :NOFOLDER
 MD %DIR%
 CD %DIR%
-git clone https://github.com/jporeilly/Partner_Workshop.git Day2
+git clone -b Day2 https://github.com/jporeilly/Partner-Workshop.git .
 EXIT
 
 
 :Item_3
-SET DIR="C:\Partner_Workshop\"
+SET DIR="C:\Partner_Workshop\Day3"
 IF EXIST %DIR% RMDIR /S /Q %DIR%\Day3
 GOTO :NOFOLDER
 IF NOT EXIST %DIR% GOTO NOFOLDER
 :NOFOLDER
 MD %DIR%
 CD %DIR%
-git clone https://github.com/jporeilly/Partner_WOrkshop.git Day3
+git clone -b Day3 https://github.com/jporeilly/Partner-Workshop.git .
 EXIT
 
 
 :Item_4
-SET DIR="C:\Partner_Workshop\"
+SET DIR="C:\Partner_Workshop\Day4"
 IF EXIST %DIR% RMDIR /S /Q %DIR%\Day4
 GOTO :NOFOLDER
 IF NOT EXIST %DIR% GOTO NOFOLDER
 :NOFOLDER
 MD %DIR%
 CD %DIR%
-git clone https://github.com/jporeilly/Partner-Workshop.git Day4
+git clone -b Day4 https://github.com/jporeilly/Partner-Workshop.git .
 EXIT
 
 
 :Item_5
 SET DIR="C:\Partner_Workshop\"
-IF EXIST %DIR% RMDIR /S /Q %DIR%\
+IF EXIST %DIR% RMDIR /S /Q %DIR%
 GOTO :NOFOLDER
 IF NOT EXIST %DIR% GOTO NOFOLDER
 :NOFOLDER
 MD %DIR%
 CD %DIR%
-git clone https://github.com/jporeilly/Partner_Workshop.git Day1
-git clone https://github.com/jporeilly/Partner_Workshop.git Day2
-git clone https://github.com/jporeilly/Partner_Workshop.git Day3
-git clone https://github.com/jporeilly/Partner_Workshop.git Day4
+https://github.com/jporeilly/Partner-Workshop.git
 EXIT
