a7ed5bb59ae3b8e165c27c9c4f0d372576d9b6c9
James O'Reilly <jp.oreilly@hotmail.com>
2018-09-10 19:04:05 +0100

    update course cmd

--- scripts/course_materials.cmd
+++ scripts/course_materials.cmd
@@ -2,16 +2,52 @@
 cls
 :start
 echo.
-echo. This script installs the required Course Materials from GitHub Repositories:
-echo.    https://github.com/jporeilly
+echo. Select an option to install the required Course Materials from GitHub Repositories.
+echo. Requires GIT to be installed and configured
 echo.
-echo.  
-echo.    1. Tesco Bank - Pentaho data Integration
-echo.        *DI-1000 - Course Files installed to C:\Pentaho-Training\Tesco-Bank
+echo. 
+echo.    1. Business Analytics - BA-1000
+echo.       * Course Files installed to C:\Pentaho-Training\BA-1000
+echo.          Pentaho User Console
+echo.          Analyzer & Interactive Reports
+echo.          Dashboards
 echo.
-echo.    
+echo.    2. Report Designer - BA-2000
+echo.       * Course Files installed to C:\Pentaho-Training\BA-2000
+echo.          Data Sources
+echo.          Elements & Groups
+echo.          Calculations
+echo.          Charts & Sub Reports
 echo.
-echo.    The window will close once scripts have successfully executed.
+echo.    3. Data Modeling - BA-3000
+echo.       * Course Files installed to C:\Pentaho-Training\BA-3000
+echo.          Metadata Editor - Models
+echo.          Schema WorkBench - ROLAP 
+echo.                  
+echo.    4. Data Integration - DI-1000
+echo.       * Course Files installed to C:\Pentaho-Training\DI-1000
+echo.          Transformations
+echo.          Dealing with Flat Files
+echo.          Database
+echo.        
+echo.    5. Advanced Data Integration - DI-1500    
+echo.       * Course Files installed to C:\Pentaho-Training\DI-1500
+echo.          Metadata Injection
+echo.          PDI as a Data Source
+echo.          Streaming Data - MQTT & Kafka
+echo.          
+echo.       * Confluent-5.0.0 installed to C:\Confluent-5.0.0
+echo.       * Project Files installed to C:\Projects
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo. The window will close once scripts have successfully executed.
 echo.
 echo.
 set /p x= Select an Option:
@@ -20,11 +56,59 @@ IF '%x%' == '%x%' GOTO Item_%x%
 
 :Item_1
 SET DIR="C:\Pentaho-Training\"
-IF EXIST %DIR% RMDIR /S /Q %DIR%\Tesco-Bank
+IF EXIST %DIR% RMDIR /S /Q %DIR%\BA-1000
 GOTO :NOFOLDER
 IF NOT EXIST %DIR% GOTO NOFOLDER
 :NOFOLDER
 MD %DIR%
 CD %DIR%
-git clone https://github.com/jporeilly/Tesco-Bank.git Tesco-Bank
+git clone -b Day1 https://github.com/jporeilly/BA-1000.git .
+EXIT
+
+
+:Item_2
+SET DIR="C:\Pentaho-Training\"
+IF EXIST %DIR% RMDIR /S /Q %DIR%\BA-2000
+GOTO :NOFOLDER
+IF NOT EXIST %DIR% GOTO NOFOLDER
+:NOFOLDER
+MD %DIR%
+CD %DIR%
+git clone -b Day2 https://github.com/jporeilly/BA-2000.git .
+EXIT
+
+
+:Item_3
+SET DIR="C:\Pentaho-Training\"
+IF EXIST %DIR% RMDIR /S /Q %DIR%\BA-3000
+GOTO :NOFOLDER
+IF NOT EXIST %DIR% GOTO NOFOLDER
+:NOFOLDER
+MD %DIR%
+CD %DIR%
+git clone -b Day3 https://github.com/jporeilly/BA-3000.git .
+EXIT
+
+
+:Item_4
+SET DIR="C:\Pentaho-Training\"
+IF EXIST %DIR% RMDIR /S /Q %DIR%\DI-1000
+GOTO :NOFOLDER
+IF NOT EXIST %DIR% GOTO NOFOLDER
+:NOFOLDER
+MD %DIR%
+CD %DIR%
+git clone -b Day4 https://github.com/jporeilly/DI-1000.git .
+EXIT
+
+
+:Item_5
+SET DIR="C:\Pentaho-Training\"
+IF EXIST %DIR% RMDIR /S /Q %DIR%\DI-1500
+GOTO :NOFOLDER
+IF NOT EXIST %DIR% GOTO NOFOLDER
+:NOFOLDER
+MD %DIR%
+CD %DIR%
+git clone https://github.com/jporeilly/DI-1500.git
 EXIT
