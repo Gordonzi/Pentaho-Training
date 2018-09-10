b7e622689eb52c490830763a929b782fcb795846
James O'Reilly <jp.oreilly@hotmail.com>
2018-09-10 21:42:42 +0100

    script updates

---
+++ scripts/DI-1000.cmd
@@ -0,0 +1,31 @@
+@echo off
+cls
+:start
+echo.
+echo. Select an option to install the required Course Materials from GitHub Repositories.
+echo. Requires GIT to be installed and configured
+echo.
+echo. 
+echo.    1. Data Integration - DI-1000
+echo.       * Course Files installed to C:\Pentaho-Training\DI-1000
+echo.          Transformations
+echo.          Dealing with Flat Files
+echo.          Database
+echo.
+echo.
+echo. The window will close once scripts have successfully executed.
+echo.
+set /p x= Select an Option:
+IF '%x%' == '%x%' GOTO Item_%x%
+
+
+:Item_1
+SET DIR="C:\Pentaho-Training\DI-1000"
+IF EXIST %DIR% RMDIR /S /Q %DIR%\DI-1000
+GOTO :NOFOLDER
+IF NOT EXIST %DIR% GOTO NOFOLDER
+:NOFOLDER
+MD %DIR%
+CD %DIR%
+git clone https://github.com/jporeilly/DI-1000.git .
+EXIT
